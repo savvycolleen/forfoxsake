667a96a5bdfccd6f1993e9b02116c54075360c28
Savannah Acevedo <140823774+savvycolleen@users.noreply.github.com>
2024-01-05 09:37:58 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const url = `https://randomfox.ca/floof/`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector("img").src = data.image
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
